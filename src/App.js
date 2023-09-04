3d7a252b2fd5b44a850f4db844db44fbb97a2993
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-04 19:15:01 +0300

    get data from OMDb API and display it on the screen

--- src/App.js
+++ src/App.js
@@ -1,77 +1,116 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import StarRating from "./StarRating";
 
-const tempMovieData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster:
-            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    },
-    {
-        imdbID: "tt0133093",
-        Title: "The Matrix",
-        Year: "1999",
-        Poster:
-            "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    },
-    {
-        imdbID: "tt6751668",
-        Title: "Parasite",
-        Year: "2019",
-        Poster:
-            "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-    },
-];
-
-const tempWatchedData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster:
-            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-        runtime: 148,
-        imdbRating: 8.8,
-        userRating: 10,
-    },
-    {
-        imdbID: "tt0088763",
-        Title: "Back to the Future",
-        Year: "1985",
-        Poster:
-            "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-        runtime: 116,
-        imdbRating: 8.5,
-        userRating: 9,
-    },
-];
+// const tempMovieData = [
+//     {
+//         imdbID: "tt1375666",
+//         Title: "Inception",
+//         Year: "2010",
+//         Poster:
+//             "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     },
+//     {
+//         imdbID: "tt0133093",
+//         Title: "The Matrix",
+//         Year: "1999",
+//         Poster:
+//             "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//     },
+//     {
+//         imdbID: "tt6751668",
+//         Title: "Parasite",
+//         Year: "2019",
+//         Poster:
+//             "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+//     },
+// ];
+//
+// const tempWatchedData = [
+//     {
+//         imdbID: "tt1375666",
+//         Title: "Inception",
+//         Year: "2010",
+//         Poster:
+//             "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//         runtime: 148,
+//         imdbRating: 8.8,
+//         userRating: 10,
+//     },
+//     {
+//         imdbID: "tt0088763",
+//         Title: "Back to the Future",
+//         Year: "1985",
+//         Poster:
+//             "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+//         runtime: 116,
+//         imdbRating: 8.5,
+//         userRating: 9,
+//     },
+// ];
 
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+const KEY = '2722a614'
 
 export default function App() {
-    const [movies, setMovies] = useState(tempMovieData);
-    const [watched, setWatched] = useState(tempWatchedData);
+    const [movies, setMovies] = useState([]);
+    const [watched, setWatched] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    useEffect(function () {
+        async function fetchMovies() {
+            try {
+                setError('')
+                setIsLoading(true)
+                const res = await fetch(`http://www.omdbapi.com/?s=harry&apikey=${KEY}`)
+                if (!res.ok) throw new Error('Something went wrong with fetching movies')
+                const movies = await res.json();
+                if (movies.Response === 'False') throw new Error('Movie not found')
+                setMovies(movies.Search)
+                setIsLoading(false)
+            } catch (err) {
+                setError(err.message)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        fetchMovies()
+    }, []);
+
     return (
         <>
-            <NavBar>
-                <Logo/>
-                <Search/>
-                <NumResults movies={movies}/>
-            </NavBar>
-            <Main>
-                <Box>
-                    <MovieList movies={movies}/>
-                </Box>
-                <Box>
-                    <WatchedSummary watched={watched}/>
-                    <WatchedMoviesList watched={watched}/>
-                </Box>
-            </Main>
-        </>
-    );
+        <NavBar>
+            <Logo/>
+            <Search/>
+            <NumResults movies={movies}/>
+        </NavBar>
+        <Main>
+            <Box>
+                {isLoading && !error && <Loader/>}
+                {!isLoading && !error && <MovieList movies={movies}/>}
+                {error && <ErrorMessage message={error}/>}
+            </Box>
+            <Box>
+                <WatchedSummary watched={watched}/>
+                <WatchedMoviesList watched={watched}/>
+        </Box>
+        </Main>
+</>
+)
+    ;
 }
+
+function Loader() {
+    return <p className='loader'>Loading...</p>
+}
+
+function ErrorMessage({message}) {
+    return <p className='error'>
+        <span>🛑</span> {message}
+    </p>
+}
+
 function NavBar({children}) {
     return <nav className="nav-bar">
         {children}
